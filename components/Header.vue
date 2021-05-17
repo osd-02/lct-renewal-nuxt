0825cf47233f01b2167765bd880d7aef0bd38257
osd <sakanaq0223@gmail.com>
2021-05-17 22:10:36 +0900

    vuetifyのtheme設定及びドロワー関連のスタイリング

---
+++ components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <v-app>
+            <v-app-bar app class="header" color="main" fixed flat>
+                <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+                <v-toolbar-title>lct-renewal</v-toolbar-title>
+                <template v-slot:extension>
+                    <v-tabs id="makeTab" background-color="sub">
+                        <v-tab>New post</v-tab>
+                        <v-tab>Articles</v-tab>
+                        <v-tab>Review</v-tab>
+                    </v-tabs>
+                </template>
+            </v-app-bar>
+            <v-navigation-drawer v-model="drawer">
+
+            </v-navigation-drawer>
+        </v-app>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            drawer: null
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .header {
+        display: fixed;
+    }
+</style>
