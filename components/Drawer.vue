a3e3d195014d2ea8d688271d2cd8c8be0946117f
osd-02 <sakanaq0223@gmail.com>
2021-06-10 10:41:45 +0900

    drawerのコンテンツを縦方向平均にならべる

--- components/Drawer.vue
+++ components/Drawer.vue
@@ -1,48 +1,93 @@
 <template>
-    <v-navigation-drawer :value="value" @input="$emit('input', $event)" fixed color="accent">
-        <v-list>
-            <v-list-item>
-                <v-list-item-content>
-                <v-list-item-title class="title">
-                    Application
-                </v-list-item-title>
-                <v-list-item-subtitle>
-                    subtext
-                </v-list-item-subtitle>
-                </v-list-item-content>
-            </v-list-item>
+  <div id="wrapper">
+    <v-navigation-drawer
+      :value="value"
+      @input="$emit('input', $event)"
+      fixed
+      color="accent"
+    >
+      <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              Contents navi
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              lct
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
 
-            <v-divider />
+      <v-divider />
 
-            <v-list-item
-            v-for="drawerItem in drawerItems"
-            :key="drawerItem.id"
-            >
-                <v-list-item-title>
-                    {{ drawerItem.title }}
-                </v-list-item-title>
-            </v-list-item>
-        </v-list>
+      <v-list id="d-wrapper">
+        <v-list-item v-for="drawerItem in drawerItems" :key="drawerItem.id">
+          <v-list-item-title>
+            {{ drawerItem.title }}
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+
+      <v-divider />
+
+      <v-list id="d-footer">
+        <v-list-item id="d-footer-item">
+          <v-list-item-content id="d-footer-item-content">
+            <v-list-item-subtitle id="d-footer-item-content-subtitle">
+              copyright ©︎lct
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
+  </div>
 </template>
-
 
 <script>
 export default {
-    data() {
-        return {
-            drawerItems: [
-                {title: "New post" ,id: 1},
-                {title: "Articles" ,id: 2},
-                {title: "Review" ,id: 3},
-                ]
-        }
-    },
-    props: {
-        value: {
-            type: Boolean,
-            default: null,
-        }
+  data() {
+    return {
+      drawerItems: [
+        { title: "New post", id: 1 },
+        { title: "Articles", id: 2 },
+        { title: "Review", id: 3 }
+      ]
+    };
+  },
+  props: {
+    value: {
+      type: Boolean,
+      default: null
     }
-}
+  }
+};
 </script>
+
+<style lang="scss" scoped>
+$title-height: 82px;
+$footer-height: 32px;
+
+#wrapper {
+    display: fixed;
+}
+
+#d-wrapper {
+  height: calc(100% - #{$title-height} - #{$footer-height});
+  display: flex;
+  flex-flow: column;
+}
+
+#d-footer {
+  margin: 0;
+  &-item {
+    min-height: 0;
+    &-content {
+      padding: 0;
+      &-subtitle {
+        text-align: right;
+      }
+    }
+  }
+}
+</style>
