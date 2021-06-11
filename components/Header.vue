d3ef3fcd7de4ec468d5ffdf6dbacee86c183c1c2
osd-02 <sakanaq0223@gmail.com>
2021-06-11 16:26:43 +0900

    Vuetifyを用いてヘッダーのtabsをモバイルとPCで表示非表示にした

--- components/Header.vue
+++ components/Header.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
-    <Drawer v-model="drawer" />
-    <v-app-bar app class="header" color="main" fixed flat>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <img src="../assets/img/logo.png" id="logo-image" />
-      <template v-slot:extension>
-        <v-tabs background-color="sub" v-model="tab" align-with-title>
-          <v-tab class="tabs" v-for="tabItem in tabItems" :key="tabItem.id">
-            {{ tabItem.title }}
-          </v-tab>
-        </v-tabs>
-      </template>
-    </v-app-bar>
+    <v-app>
+      <Drawer v-model="drawer" />
+      <v-app-bar app class="header" color="main" fixed flat>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <img src="../assets/img/logo.png" id="logo-image" />
+        <template v-slot:extension v-if="tabShow">
+          <v-tabs background-color="sub" align-with-title>
+            <v-tab class="tabs" v-for="tabItem in tabItems" :key="tabItem.id">
+              {{ tabItem.title }}
+            </v-tab>
+          </v-tabs>
+        </template>
+      </v-app-bar>
+    </v-app>
   </div>
 </template>
 
@@ -19,6 +21,16 @@
 import Drawer from "./Drawer.vue";
 
 export default {
+  computed: {
+    tabShow() {
+      switch (this.$vuetify.breakpoint.name) {
+        case "xs":
+          return false;
+        case "sm":
+          return true;
+      }
+    }
+  },
   components: {
     Drawer
   },
