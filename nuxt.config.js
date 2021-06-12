5038b0aca9329c4bb513fcf1eb12e209e4e2991c
osd-02 <sakanaq0223@gmail.com>
2021-06-13 01:14:18 +0900

    Flamelink通した，かなりえらいです

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,64 +1,59 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'lct-nuxt',
+    title: "lct-nuxt",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
-  
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-  
+  css: [],
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-  
+  plugins: [],
+
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-  
+
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    "@nuxt/typescript-build",
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss",
     // https://vuetifyjs.com/ja/
-    '@nuxtjs/vuetify',
+    "@nuxtjs/vuetify"
   ],
-  
+
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-  
+  modules: [],
+
   vuetify: {
     theme: {
       light: true,
       themes: {
         light: {
-          main: '#d7c1a3',
-          sub: '#ded3bb',
-          primary: '#d14126',
-          secondary: '#ccc8bf',
-          accent: '#bdd3e2',
-          warning: '#aa9e32',
-          success: '#0f4c81'
-        },
-      },
-    },
+          main: "#d7c1a3",
+          sub: "#ded3bb",
+          primary: "#d14126",
+          secondary: "#ccc8bf",
+          accent: "#bdd3e2",
+          warning: "#aa9e32",
+          success: "#0f4c81"
+        }
+      }
+    }
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+
+  plugins: ["@/plugins/flamelink"]
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,41 @@
 <template>
   <v-sheet id="wrapper">
-    <v-sheet width="100%" min-height="130vh" color="secondary">
-      <h2>lct-newal</h2>
+    <v-sheet width="100%" min-height="100vh" color="secondary">
+      <v-subheader>
+        News
+      </v-subheader>
+      <v-card class="mx-auto" width="80vw" rounded elevation="6" color="sub">
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>Two-line item</v-list-item-title>
+            <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
+      <v-card class="mx-auto" width="80vw" rounded elevation="6" color="sub">
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>Two-line item</v-list-item-title>
+            <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
+      <v-card class="mx-auto" width="80vw" rounded elevation="6" color="sub">
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>Two-line item</v-list-item-title>
+            <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
+      <v-card class="mx-auto" width="80vw" rounded elevation="6" color="sub">
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>Two-line item</v-list-item-title>
+            <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
     </v-sheet>
   </v-sheet>
 </template>
@@ -11,7 +45,20 @@ import Vue from "vue";
 import Drawer from "~/components/Drawer.vue";
 
 export default Vue.extend({
-  components: { Drawer }
+  components: { Drawer },
+  async asyncData({ app }) {
+    try {
+      const baths = await app.flamelink.content.get({
+        schemaKey: "baths", // Flamelinkで設定したschemeID
+        populate: true
+      });
+      console.log({ baths });
+      return { baths };
+    } catch (err) {
+      console.log(err);
+      return { myposts: [] };
+    }
+  }
 });
 </script>
 
@@ -24,7 +71,7 @@ $header-height: 106px;
   // position: relative;
   // padding-top: 30vh;
   // top: -30vh;
-  
+
   #header-space {
     height: $header-height;
     padding: 0;
