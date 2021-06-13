3e40c0f234049d7893b574daed93178c970dd5be
osd-02 <sakanaq0223@gmail.com>
2021-06-13 19:30:16 +0900

    firebaseに起こっていたバグ解消・及びfirebaseの設定

--- plugins/flamelink.js
+++ plugins/flamelink.js
@@ -1,4 +1,5 @@
 import flamelink from "flamelink/app";
+import { firebase } from '@firebase/app'
 // This example uses RTDB (Realtime Database) - replace with `cf` for Cloud Firestore
 import "flamelink/cf/content";
 import "flamelink/cf/storage";
@@ -13,19 +14,20 @@ export default ({ app }) => {
     const admin = require("firebase-admin");
 
     if (!admin.apps.length) {
-      const serviceAccount = require('../serviceAccountKey.json')
+      const serviceAccount = require(process.env
+        .FLAMELINK_PATH_TO_SERVICE_ACCOUNT);
 
       firebaseApp = admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
+        databaseURL: process.env.FLAMELINK_DB_URL,
         storageBucket: process.env.FLAMELINK_STORAGE_BUCKET
       });
     } else {
       firebaseApp = admin.app();
     }
   } else {
-    const firebase = require("firebase/app");
-    // require('firebase/auth')
-    // require('firebase/firestore')
+    require("firebase/auth");
+    require("firebase/firestore");
     require("firebase/database");
     require("firebase/storage");
 
@@ -33,6 +35,7 @@ export default ({ app }) => {
       firebaseApp = firebase.initializeApp({
         apiKey: process.env.FLAMELINK_API_KEY,
         authDomain: process.env.FLAMELINK_AUTH_DOMAIN,
+        databaseURL: process.env.FLAMELINK_DB_URL,
         projectId: process.env.FLAMELINK_PROJECT_ID,
         storageBucket: process.env.FLAMELINK_STORAGE_BUCKET,
         messagingSenderId: process.env.FLAMELINK_MESSAGING_SENDER_ID
