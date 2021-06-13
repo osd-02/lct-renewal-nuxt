2b959599cb7a113ce6b8d4631cf5a693c76a5ed4
osd-02 <sakanaq0223@gmail.com>
2021-06-13 23:33:06 +0900

    Flamelinkにデモで記事を追加及びニュース欄に表示

--- pages/index.vue
+++ pages/index.vue
@@ -4,35 +4,23 @@
       <v-subheader>
         News
       </v-subheader>
-      <v-card class="mx-auto" width="80vw" rounded elevation="6" color="sub">
+      <v-card
+        class="mx-auto news"
+        width="80vw"
+        rounded
+        elevation="6"
+        color="sub"
+        v-for="article in articles"
+        v-bind:key="article.publishDate"
+      >
         <v-list-item two-line>
           <v-list-item-content>
-            <v-list-item-title>Two-line item</v-list-item-title>
-            <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-card>
-      <v-card class="mx-auto" width="80vw" rounded elevation="6" color="sub">
-        <v-list-item two-line>
-          <v-list-item-content>
-            <v-list-item-title>Two-line item</v-list-item-title>
-            <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-card>
-      <v-card class="mx-auto" width="80vw" rounded elevation="6" color="sub">
-        <v-list-item two-line>
-          <v-list-item-content>
-            <v-list-item-title>Two-line item</v-list-item-title>
-            <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-card>
-      <v-card class="mx-auto" width="80vw" rounded elevation="6" color="sub">
-        <v-list-item two-line>
-          <v-list-item-content>
-            <v-list-item-title>Two-line item</v-list-item-title>
-            <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+            <v-list-item-title
+              >{{ article.title }} / {{ article.type }}</v-list-item-title
+            >
+            <v-list-item-subtitle>{{
+              article.announceBody
+            }}</v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
       </v-card>
@@ -48,37 +36,23 @@ export default Vue.extend({
   components: { Drawer },
   async asyncData({ app }) {
     try {
-      const baths = await app.flamelink.content.get({
-        schemaKey: "baths",
+      const articles = await app.flamelink.content.get({
+        schemaKey: "articles",
         populate: true
       });
-      console.log({ baths });
-      return { baths };
+      return { articles };
     } catch (err) {
       console.log(err);
       return { myposts: [] };
     }
-  }
+  },
 });
 </script>
 
 <style lang="scss" scoped>
 $header-height: 106px;
 
-#wrapper {
-  // min-height: 130vh;
-  // width: 100%;
-  // position: relative;
-  // padding-top: 30vh;
-  // top: -30vh;
-
-  #header-space {
-    height: $header-height;
-    padding: 0;
-  }
-  #body {
-    padding: 20px;
-    // background-color: "sub";
-  }
+.news {
+  margin: 20px;
 }
 </style>
