252c5c064ab2da6d74c80ab293583d0ac3340a15
osd-02 <sakanaq0223@gmail.com>
2021-06-28 21:28:28 +0900

    エラー解消！emailの設定がおかしかった

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
   <v-sheet id="wrapper">
-    <v-sheet width="100%" min-height="100vh" color="secondary" id="home-location">
-      <News :data="this.articles" id="news-location"/>
-      <Articles :data="this.articles" id="articles-location"/>
-      <Profile :data="this.profile" id="profile-location"/>
+    <v-sheet
+      width="100%"
+      min-height="100vh"
+      color="secondary"
+      id="home-location"
+    >
+      <News :data="this.articles" id="news-location" />
+      <Articles :data="this.articles" id="articles-location" />
+      <Profile :data="this.profile" id="profile-location" />
     </v-sheet>
   </v-sheet>
 </template>
@@ -42,6 +47,5 @@ $header-height: 106px;
   //   //   // float: right;
   //   // }
   // }
-
 }
 </style>
