9996039f8ab8304ee523b0a4b5f89a188e32eaa6
osd-02 <sakanaq0223@gmail.com>
2021-07-02 05:01:17 +0900

    コメントアウトの消去

--- pages/index.vue
+++ pages/index.vue
@@ -22,9 +22,6 @@ function formatTime (object, after) {
         object[property].publishDate = object[property].publishDate.slice(0, -6).replace(/-/g, after)
       }
 }
-// for (const property in articles) {
-//         articles[property].publishDate = articles[property].publishDate.slice(0, -6).replace(/-/g, '/')
-//       }
 export default Vue.extend({
   components: { Drawer },
   async asyncData({ app }) {
