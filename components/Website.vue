063460b6a602a615335693ca261b384ccb78a5f0
osd-02 <sakanaq0223@gmail.com>
2021-09-06 17:49:09 +0900

    websiteのコンポーネントの実装

--- components/Website.vue
+++ components/Website.vue
@@ -1,15 +1,25 @@
 <template>
   <v-sheet id="wrapper" color="sub">
-    <v-container fill-height fluid class="p-0">
-      <v-row no-gutters class="block">
-        <v-sheet color="success" class="text-xl text-white pl-6 font-Spaciouo">
-          <h5>Website</h5>
-        </v-sheet>
-      </v-row>
-      <v-row no-gutters class="pl-3">
-        Suisei HP
-      </v-row>
-    </v-container>
+    <v-sheet color="success" class="text-xl text-white pl-6 font-Spaciouo">
+      <h5>Website</h5>
+    </v-sheet>
+    <v-sheet color="sub">
+      <v-list dense color="secondary">
+      <v-list-item-group
+        color="primary"
+      >
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :href=item.link
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+    </v-sheet>
   </v-sheet>
 </template>
 
@@ -17,7 +27,12 @@
 import Vue from "vue";
 
 export default Vue.extend({
-
+  data: () => ({
+      items: [
+        { text: 'Suisei HP', link: 'https://suisei.herokuapp.com' },
+        { text: '旧Lo-culture', link: 'http://lct.starfree.jp' },
+      ],
+    }),
 });
 </script>
 
